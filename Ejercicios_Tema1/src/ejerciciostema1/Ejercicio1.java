34ed04d6fb22107a336604a5cf763713939a2ca7
jsevilla202 <jsevilla@iesnervion.es>
2022-09-29 13:38:58 +0200

    Ejercicios Tema 1

--- Ejercicios_Tema1/src/ejerciciostema1/Ejercicio1.java
+++ Ejercicios_Tema1/src/ejerciciostema1/Ejercicio1.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
+		//Nombramos la variable "numero" como double para obtener los decimales
 		double numero;
-		
+		//Creamos el scanner llamado "sc"
 		Scanner sc = new Scanner(System.in);
-		
+		//Pedimos al usuario que inserte el numero con decimales
 		System.out.print("Inserte un número con decimales que quiera redondear al numero entero "
 				+ "más proximo: ");
+		//Capturamos lo que ha introducido el usuario y lo guardamos en la variable "numero"
 		numero = sc.nextDouble();
-
-		System.out.print((int)numero);
+		/*Truncamos el numero con (int) pero se le suman decimales dependiendo de lo buenos
+		que seamos al corregir Ej. si subimos a partir de un 0.4 se le añade 0.6 al numero
+		introducido*/ 
+		System.out.print((int)(numero+0.5));
 	}
 
 }
