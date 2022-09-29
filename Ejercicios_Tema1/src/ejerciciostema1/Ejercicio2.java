2030eaee9dc0b8ca0aaa2c4cb25066e4bacbd182
jsevilla202 <jsevilla@iesnervion.es>
2022-09-29 13:53:52 +0200

    Ejercicios Tema 1

--- Ejercicios_Tema1/src/ejerciciostema1/Ejercicio2.java
+++ Ejercicios_Tema1/src/ejerciciostema1/Ejercicio2.java
@@ -1,10 +1,25 @@
 package ejerciciostema1;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int numero, resto, suma;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Inserte el número que desee para saber cuantos numero le hacen falta "
+				+ "para que sea multiplo de 7: ");
+		
+		numero = sc.nextInt();
+		resto = (numero%7);
+		suma = 7 - resto;
+				
+		System.out.print(resto==0 ? "Su número ya es multiplo de 7" : "Si a su número se le suma "
+			+suma +" seía multiplo de 7");
+		
+		sc.close();
 	}
 
 }
